9f2ac61853fb1d280ebd70a38c087ff5b72ef47f
Tomas Edson <t.edson.almeida@hotmail.com>
2022-02-07 20:31:46 -0300

    config animação nos itens do menu

--- js/index.js
+++ js/index.js
@@ -1,8 +1,28 @@
 function ativa_menu(){
     const obj = document.querySelector('#lista_menu');
     const classes = obj.classList;
+
+    //Adiciona a classe que faz aparecer a ul 
+    classes.toggle('menu_lista-ativa');
+    //Armazena o número de links filhos
+    const filhos = obj.children.length;
+
+    //Percorre os links e adiciona a classe com a animação
+    for(i=1; i <= filhos; i++){
+
+        let id_nome = 'menu_link' + i;
+        const obj_link = document.querySelector('#' + id_nome);
+        const classes_link = obj_link.classList;
+        classes_link.toggle('anima_link');
+
+    }
+
    
 
-    classes.toggle('menu_lista-ativa');
+//    alert(filhos);
+    
+   
 
 }
+
+//menu_lista_link4
